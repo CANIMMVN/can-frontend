d7f433cfa4a6b2390252fe719e5754dd19a44d5b
Huy Pham <pham.giahuy.9041@gmail.com>
2023-03-30 06:29:00 +0700

    CAN-61 Update use fs to create new directoy to prepare for multer access.

--- src/middlewares/handleFiles.ts
+++ src/middlewares/handleFiles.ts
@@ -1,18 +1,28 @@
 import multer from "multer";
 import { MAX_CONTENT_FILE_SIZE, MAX_NUMBER_FILE } from "../utils/settings/setting";
+import path from "path";
+import fs from "fs";
 
-export default (fileFromName: string) =>
-	multer({
+export default (fileFromName: string) => {
+	const uploadDirectory = path.join(__dirname, "uploads");
+	if (!fs.existsSync(uploadDirectory)) {
+		fs.mkdir(uploadDirectory, (err) => {
+			if (err) throw err;
+			console.log("Directory created successfully!");
+		});
+	}
+	return multer({
 		limits: {
 			fileSize: MAX_CONTENT_FILE_SIZE,
 			files: MAX_NUMBER_FILE,
 		},
 		storage: multer.diskStorage({
 			destination: (req, file, cb) => {
-				cb(null, "./public/uploads");
+				cb(null, uploadDirectory);
 			},
 			filename: (req, file, cb) => {
 				cb(null, file.originalname);
 			},
 		}),
 	}).array(fileFromName);
+};
