4bd197c50115f236bbf39415dbcff084c8b50dfc
Huy Pham <pham.giahuy.9041@gmail.com>
2023-05-08 06:33:53 +0700

    CAN-81 Add new way to assert type to types and tsdoc note.

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -7,7 +7,19 @@ export type TComponent<T = undefined> = T extends undefined ? { (): JSX.Element 
  */
 export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
 
+/**
+ * This function doesn't do any runtime checks, it's just assert the type of the input parameter.
+ */
 export const Assert = <T>(element: T) => element;
+
+/**
+ * If the assertion passes, it does nothing.
+ *
+ * If the assertion fails, a runtime error is thrown.
+ *
+ * This function is useful when need to ensure that a value is of a certain type before proceeding with further operations.
+ */
+export function _Assert<T>(v: any): asserts v is T {}
 
 export enum Direction {
 	"Left",
