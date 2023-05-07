e1378cc9d6df2b8c4e962699fae5e6f2616fc8b5
Huy Pham <pham.giahuy.9041@gmail.com>
2023-05-08 06:33:07 +0700

    CAN-81 Add utility type DistributiveOmit to types.

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,12 @@
 export type TComponent<T = undefined> = T extends undefined ? { (): JSX.Element } : { (props: T): JSX.Element };
 
+/**
+ * DistributiveOmit credit goes to jcalz
+ *
+ * Link the community https://stackoverflow.com/questions/57103834/typescript-omit-a-property-from-all-interfaces-in-a-union-but-keep-the-union-s
+ */
+export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
+
 export const Assert = <T>(element: T) => element;
 
 export enum Direction {
